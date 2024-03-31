cebb5c2f838060abf15e05426caa56abd2dc1e8d
Akash kr <akashkr971@gmail.com>
2024-03-31 17:53:07 +0530

    added git commands

--- akapro/akapro/settings.py
+++ akapro/akapro/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'akapro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'household',
+        'USER' : 'root',
+        'PASSWORD' : 'akashkr',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
