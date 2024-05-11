444348abe51b92d791ff57c0aaafa36291f3f56a
Akash kr <akashkr971@gmail.com>
2024-05-12 00:40:24 +0530

    Update settings.py

--- hmspro/settings.py
+++ hmspro/settings.py
@@ -129,11 +129,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'akashkr971@gmail.com'
-EMAIL_HOST_PASSWORD = 'hyug ecjf jvfr rnzt'
-EMAIL_USE_TLS = True
-
-DEFAULT_FROM_EMAIL = 'akashkr971@gmail.com'
